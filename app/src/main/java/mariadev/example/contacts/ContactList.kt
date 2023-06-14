19bd133a928d4d2b3e33ed84f2cf1b71b1289441
Mariagoretti <mariakimanigoreti@gmail.com>
2023-06-14 17:20:26 +0300

    Contact-list

--- app/src/main/java/mariadev/example/contacts/ContactList.kt
+++ app/src/main/java/mariadev/example/contacts/ContactList.kt
@@ -1,7 +1,7 @@
 package mariadev.example.contacts
 
 data class ContactList(
-    val name:String,
+    val name: String,
     val phoneNumber: String,
     val email: String,
     val ImagePlaceholder: String
